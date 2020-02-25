37baf414782bc0a5e6886e2761a9d73eed38f357
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-25 20:14:46 -0300

    Instaciação

--- src/main/java/com/lopes/curso/CursomcApplication.java
+++ src/main/java/com/lopes/curso/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.lopes.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.lopes.curso.domain.Categoria;
+import com.lopes.curso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		repository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/lopes/curso/services/CategoriaService.java
+++ src/main/java/com/lopes/curso/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.lopes.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,6 @@ public class CategoriaService {
 		Optional<Categoria> categoria = repository.findById(id);
 		return categoria.orElse(null);
 	}
+	
 	
 }
