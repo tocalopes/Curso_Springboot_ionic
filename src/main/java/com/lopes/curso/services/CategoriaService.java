7957b9edb5aebb2ee022c684a9330edde9aee21a
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-25 20:05:29 -0300

    Adição das camadas de service e repository de categoria

---
+++ src/main/java/com/lopes/curso/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.lopes.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lopes.curso.domain.Categoria;
+import com.lopes.curso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> categoria = repository.findById(id);
+		return categoria.orElse(null);
+	}
+	
+}
